fb60fda9caa4bb37d82cd4eedf69717940dfd2e9
DevilGuru <Shubhamverma6772@gmail.com>
2023-04-27 02:39:18 +0530

    Bug-Fixing

--- src/componants/heroBanner/HeroBanner.jsx
+++ src/componants/heroBanner/HeroBanner.jsx
@@ -53,7 +53,7 @@ const HeroBanner = () => {
                 onKeyUp={searchQueryHandler}
                 onChange={(e) => setQuery(e.target.value)}
               />
-              <button>Search</button>
+              <button  onClick={()=> navigate(`/search/${query}`)}>Search</button>
             </div>
           </div>
         </div>
